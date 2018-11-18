5b9aaaf0ac288953c792a31e8597d3691d96c1aa
Layth Al-Qattan <laythq@gmail.com>
2018-11-18 13:08:03 +0000

    Added <ul> nesting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TreeNode from './TreeNode'
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    var todoList = {
+      title: 'one',
+      childNodes: [
+        {
+          title: 'one',
+          childNodes: [
+          {
+            title: 'one',
+            childNodes: []
+          }
+        ]},
+        {
+          title: 'two',
+          childNodes: []
+        }
+      ]
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <TreeNode node={todoList}/>
       </div>
-    );
+    )
   }
 }
 
--- src/TreeNode.js
+++ src/TreeNode.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 export default class TreeNode extends React.Component {
   render() {
-    const childNodes = this.props.node.childNodes.map(function(child, index) {
-      return <li key={index}><TreeNode node={index} /></li>
+    var childNodes
+    if (this.props.node.childNodes != null){
+      childNodes = this.props.node.childNodes.map(function(child, index) {
+        return <li key={index}><TreeNode node={child} /></li>
+      }
+      )
     }
-    )
     return (
       <div>
+        {this.props.node.title}
         <ul>
           {childNodes}
         </ul>
-
       </div>
     )
   }
